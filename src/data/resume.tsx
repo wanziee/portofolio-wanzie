7ec5318d1f02c83a59931a453fe370a198437086
wanziee <ichwanalghifa@gmail.com>
2025-09-22 21:38:51 +0700

    adding my 4th project and rearrange my projects order

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -5,6 +5,7 @@ import Bsi from "../assets/logo-bsi.png";
 import Telkom from "../assets/telkom.jpg";
 import Video from "../assets/videoProject.mp4";
 import Project3 from "../assets/project3.jpg";
+import Project4 from "../assets/project4.jpg";
 
 export const DATA = {
   projects: [
@@ -13,7 +14,7 @@ export const DATA = {
       dates: "July 2024",
       description:
         "Gameload is a fast and secure game top-up website. Choose your game, enter your ID, and get your items instantly.",
-      technologies: ["Laravel", "Php", "API Midtrans"],
+      technologies: ["Laravel", "Php", "Midtrans API"],
       links: [
         {
           type: "Github",
@@ -73,8 +74,40 @@ export const DATA = {
       video: "",
     },
     {
+    title: "CoinTrek",
+    dates: "July 2025",
+    description:
+      "CoinTrek is a cryptocurrency tracker app built with SwiftUI that integrates the CoinGecko API to display real-time prices, charts, and market data.",
+    technologies: ["Swift", "SwiftUI", "CoinGecko API"],
+    links: [
+      {
+        type: "Github",
+        href: "https://github.com/wanziee/CoinTrek",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="13"
+            height="13"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-github-icon lucide-github"
+          >
+            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+            <path d="M9 18c-4.51 2-5-2-7-2" />
+          </svg>
+        ),
+      },
+    ],
+    image: Project3,
+    video: "",
+  },
+    {
     title: "Cepat Tanggap",
-    dates: "June 2025 - Now",
+    dates: "September 2025 - Now",
     description:
       "Cepat Tanggap is a citizen reporting app that allows users to submit reports, track status, and receive responses from admins in real-time.",
     technologies: ["Swift","SwiftUI", "Node.js", "Express", "Sequelize", "React", "RESTful API"],
@@ -101,9 +134,10 @@ export const DATA = {
         ),
       },
     ],
-    image: Project3,
+    image: Project4,
     video: "",
   },
+  
   ],
 
   educations: [
